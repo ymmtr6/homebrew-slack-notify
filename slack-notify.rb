bf9b8063e9b81fa9c8656d25da43990a5168c1d5
Riku Yamamoto <ymmtr666@gmail.com>
2020-09-12 00:05:09 +0900

    update 0.0.2

--- slack-notify.rb
+++ slack-notify.rb
@@ -1,11 +1,12 @@
 REPOSITORY_URL="https://github.com/ymmtr6/slack-notify".freeze
-HOMEBREW_SLACK_NOTIFY_VERSION="0.0.1".freeze
+HOMEBREW_SLACK_NOTIFY_VERSION="0.0.2".freeze
 
 class SlackNotify < Formula
   desc "transfer stdin or argument using slack incomming webhook."
   homepage REPOSITORY_URL
+  version HOMEBREW_SLACK_NOTIFY_VERSION
   url "#{REPOSITORY_URL}/archive/#{HOMEBREW_SLACK_NOTIFY_VERSION}.tar.gz"
-  sha256 "07ddd3fd1d0b0f10d6c2556239b5951fa78bcf9ef772841d1d5686d57b04ff3b"
+  sha256 "b96d14b5a311275a53abb27efdebb35522cc596a68d24029e1a6665c0d3025bc"
   head "#{REPOSITORY_URL}.git", :tag => HOMEBREW_SLACK_NOTIFY_VERSION
 
   def install
