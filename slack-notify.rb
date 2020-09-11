a2f5b50e70c75e7aab4d15448017227eba2abc76
Riku Yamamoto <ymmtr666@gmail.com>
2020-09-11 22:03:49 +0900

    bugfix

--- slack-notify.rb
+++ slack-notify.rb
@@ -9,7 +9,7 @@ class SlackNotify < Formula
   desc "transfer stdin or argument using slack incomming webhook."
   homepage "https://github.com/ymmtr6/slack-notify"
   license "MIT"
-  url "#{REPOSITORY_URL}/archive/#{HOMEBREW_GITPULLS_VERSION}.tar.gz"
+  url "#{REPOSITORY_URL}/archive/#{HOMEBREW_SLACK_NOTIFY_VERSION}.tar.gz"
   sha256 "07ddd3fd1d0b0f10d6c2556239b5951fa78bcf9ef772841d1d5686d57b04ff3b"
   head "https://github.com/ymmtr6/slack-notify.git", :tag => HOMEBREW_SLACK_NOTIFY_VERSION
 
