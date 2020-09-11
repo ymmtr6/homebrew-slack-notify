a2f93c8335c26b90e841ffcc1d4b7e17de0e4398
Riku Yamamoto <ymmtr666@gmail.com>
2020-09-11 22:14:01 +0900

    fix

--- slack-notify.rb
+++ slack-notify.rb
@@ -3,13 +3,10 @@ HOMEBREW_SLACK_NOTIFY_VERSION="0.0.1".freeze
 
 class SlackNotify < Formula
   desc "transfer stdin or argument using slack incomming webhook."
-  homepage "https://github.com/ymmtr6/slack-notify"
-  license "MIT"
+  homepage REPOSITORY_URL
   url "#{REPOSITORY_URL}/archive/#{HOMEBREW_SLACK_NOTIFY_VERSION}.tar.gz"
   sha256 "07ddd3fd1d0b0f10d6c2556239b5951fa78bcf9ef772841d1d5686d57b04ff3b"
   head "#{REPOSITORY_URL}.git", :tag => HOMEBREW_SLACK_NOTIFY_VERSION
-
-  # depends_on "cmake" => :build
 
   def install
     bin.install "bin/slack-notify"
